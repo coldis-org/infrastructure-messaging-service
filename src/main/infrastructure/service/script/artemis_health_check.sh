58427eec57e6afa84bdcbb2806096dcfb7a460ac
rvcoutinho <me@rvcoutinho.com>
2020-10-04 21:54:53 -0300

    - Upgrading version;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

---
+++ src/main/infrastructure/service/script/artemis_health_check.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Adds a message to the expiry queue.
+POST_RESPONSE=$(curl -f -s -u ${ARTEMIS_USERNAME}:${ARTEMIS_PASSWORD} -X POST -H "Origin: localhost" \
+	-H "Content-Type: application/json" "http://localhost:8161/console/jolokia/exec" -d \
+"{\
+	\"type\": \"EXEC\", \
+	\"mbean\": \"org.apache.activemq.artemis:broker=\\\"0.0.0.0\\\",component=addresses,address=\\\"ExpiryQueue\\\",subcomponent=queues,routing-type=\\\"anycast\\\",queue=\\\"ExpiryQueue\\\"\", \
+	\"operation\": \"sendMessage(java.util.Map,int,java.lang.String,boolean,java.lang.String,java.lang.String)\", \
+	\"arguments\": [ {}, 1, \"test\", false, \"${ARTEMIS_USERNAME}\", \"${ARTEMIS_PASSWORD}\" ]\
+}")
+#echo ${POST_RESPONSE}
+POST_MESSAGE_ID=$(echo ${POST_RESPONSE} | jq ".value")
+POST_RESPONSE_STATUS=$(echo ${POST_RESPONSE} | jq ".status")
+echo "Post status: ${POST_RESPONSE_STATUS}"
+if [ "${POST_RESPONSE_STATUS}" != "200" ]
+then
+	exit "Message could not be added. Status: ${POST_RESPONSE_STATUS}"
+fi
+
+# Removes the test message from the queue.
+DELETE_RESPONSE=$(curl -f -s -u ${ARTEMIS_USERNAME}:${ARTEMIS_PASSWORD} -X POST -H "Origin: localhost" \
+	-H "Content-Type: application/json" "http://localhost:8161/console/jolokia/exec" -d \
+"{\
+	\"type\": \"EXEC\", \
+	\"mbean\": \"org.apache.activemq.artemis:broker=\\\"0.0.0.0\\\",component=addresses,address=\\\"ExpiryQueue\\\",subcomponent=queues,routing-type=\\\"anycast\\\",queue=\\\"ExpiryQueue\\\"\", \
+	\"operation\": \"removeMessage(long)\", \
+	\"arguments\": [ ${POST_MESSAGE_ID} ]\
+}")
+#echo ${DELETE_RESPONSE}
+DELETE_RESPONSE_STATUS=$(echo ${DELETE_RESPONSE} | jq ".status")
+echo "Delete status: ${DELETE_RESPONSE_STATUS}"
+if [ "${DELETE_RESPONSE_STATUS}" != "200" ]
+then
+	exit "Message could not be deleted. Status: ${DELETE_RESPONSE_STATUS}"
+fi
+
+
---
+++ src/main/infrastructure/service/script/artemis_init.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+set -e
+
+BROKER_HOME=/var/lib/artemis
+CONFIG_PATH=$BROKER_HOME/etc
+export BROKER_HOME CONFIG_PATH
+
+# Log to tty to enable docker logs container-name
+sed -i "s/logger.handlers=.*/logger.handlers=CONSOLE/g" ${CONFIG_PATH}/logging.properties
+
+# Update users and roles with if username and password is passed as argument
+if [ "$ARTEMIS_USERNAME" ] && [ "$ARTEMIS_PASSWORD" ]; then
+	$BROKER_HOME/bin/artemis user rm --user artemis
+	$BROKER_HOME/bin/artemis user add --user "$ARTEMIS_USERNAME" --password "$ARTEMIS_PASSWORD" --role amq
+fi
+
+# Runs performance journal.
+performanceJournal() {
+	perfJournalConfiguration=${ARTEMIS_PERF_JOURNAL:-AUTO}
+	if [ "$perfJournalConfiguration" = "AUTO" ] || [ "$perfJournalConfiguration" = "ALWAYS" ]; then
+
+		if [ "$perfJournalConfiguration" = "AUTO" ] && [ -e /var/lib/artemis/data/.perf-journal-completed ]; then
+			echo "Volume's journal buffer already fine tuned"
+			return
+		fi
+
+		echo "Calculating performance journal ... "
+		RECOMMENDED_JOURNAL_BUFFER=$("./artemis" "perf-journal" | grep "<journal-buffer-timeout" | xmlstarlet sel -t -c '/journal-buffer-timeout/text()' || true)
+		if [ -z "$RECOMMENDED_JOURNAL_BUFFER" ]; then
+			echo "There was an error calculating the performance journal, gracefully handling it"
+			return
+		fi
+
+		xmlstarlet ed -L \
+			-N activemq="urn:activemq" \
+			-N core="urn:activemq:core" \
+			-u "/activemq:configuration/core:core/core:journal-buffer-timeout" \
+			-v "$RECOMMENDED_JOURNAL_BUFFER" ../etc/broker.xml
+			echo "$RECOMMENDED_JOURNAL_BUFFER"
+
+		if [ "$perfJournalConfiguration" = "AUTO" ]; then
+			touch /var/lib/artemis/data/.perf-journal-completed
+		fi
+	else
+		echo "Skipping performance journal tuning as per user request"
+	fi
+}
+if (echo "${ACTIVEMQ_ARTEMIS_VERSION}" | grep -Eq	"(1.5\\.[^12]|[^1]\\.[0-9]+\\.[0-9]+)" ) ; then 
+	performanceJournal
+else
+	echo "Ignoring any performance journal parameter as version predates it: ${ACTIVEMQ_ARTEMIS_VERSION}"
+fi
+
+# Runs Artemis.
+exec "$@"
