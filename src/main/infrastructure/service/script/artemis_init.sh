7f64226bc82bc467be161d190bd37fa3105d20fb
rvcoutinho <me@rvcoutinho.com>
2022-12-24 10:33:18 -0300

    - Upgrading;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/infrastructure/service/script/artemis_init.sh
+++ src/main/infrastructure/service/script/artemis_init.sh
@@ -10,6 +10,9 @@ BROKER_HOME=/var/lib/artemis
 CONFIG_PATH=$BROKER_HOME/etc
 export BROKER_HOME CONFIG_PATH
 
+ENV_VARIABLES=$(awk 'BEGIN{for(v in ENVIRON) print "$"v}')
+envsubst "$ENV_VARIABLES" <"${CONFIG_PATH}/broker.xml" | sponge "${CONFIG_PATH}/broker.xml"
+
 # Update users and roles with if username and password is passed as argument
 if [ "${ARTEMIS_USERNAME}" ] && [ "${ARTEMIS_PASSWORD}" ]; then
 	echo "${ARTEMIS_USERNAME} = ${ARTEMIS_PASSWORD}" > ${CONFIG_PATH}/artemis-users.properties
