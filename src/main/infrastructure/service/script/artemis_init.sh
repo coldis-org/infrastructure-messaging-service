79ba43132437f064ca8116eb293a3c00e0c3b647
rvcoutinho <me@rvcoutinho.com>
2023-08-03 11:57:20 -0300

    - Configuring global max size and queue size;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/infrastructure/service/script/artemis_init.sh
+++ src/main/infrastructure/service/script/artemis_init.sh
@@ -10,8 +10,29 @@ BROKER_HOME=/var/lib/artemis
 CONFIG_PATH=$BROKER_HOME/etc
 export BROKER_HOME CONFIG_PATH
 
+# Max memory and global size and queue size.
+if [ -z "${MAX_MEMORY}" ]
+then
+	MAX_MEMORY=$(cat "/sys/fs/cgroup/memory/memory.limit_in_bytes" || echo 0)
+	if ! [ ${MAX_MEMORY} -ne 0 ]
+	then
+		MAX_MEMORY=$((1024 * 1024 * 1024))
+	fi
+fi
+
+QUEUE_MAX_SIZE=$(echo $((MAX_MEMORY / MAX_QUEUE_SIZE_RATIO)))
+GLOBAL_MAX_SIZE=$(echo $((MAX_MEMORY / MAX_GLOBAL_SIZE_RATIO)))
+
+export QUEUE_MAX_SIZE
+export GLOBAL_MAX_SIZE
+
+echo MAX_MEMORY=${MAX_MEMORY}
+echo QUEUE_MAX_SIZE=${QUEUE_MAX_SIZE}
+echo GLOBAL_MAX_SIZE=${GLOBAL_MAX_SIZE}
+
 ENV_VARIABLES=$(awk 'BEGIN{for(v in ENVIRON) print "$"v}')
 envsubst "$ENV_VARIABLES" <"${CONFIG_PATH}/broker.xml" | sponge "${CONFIG_PATH}/broker.xml"
+cat "${CONFIG_PATH}/broker.xml"
 
 # Update users and roles with if username and password is passed as argument
 if [ "${ARTEMIS_USERNAME}" ] && [ "${ARTEMIS_PASSWORD}" ]; then
