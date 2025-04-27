a42251fb4d7c7d4af0bcc642e4832599e92726fb
rvcoutinho <me@rvcoutinho.com>
2025-04-27 13:07:00 -0300

    - Tuning; Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/infrastructure/service/script/artemis_health_check.sh
+++ src/main/infrastructure/service/script/artemis_health_check.sh
@@ -1,49 +1,40 @@
 #!/bin/sh
 
 # Adds a message to the expiry queue.
-POST_RESPONSE=$(curl -f -s -u ${ARTEMIS_USERNAME}:${ARTEMIS_PASSWORD} -X POST -H "Origin: localhost" \
-	-H "Content-Type: application/json" "http://localhost:8161/console/jolokia/exec" -d \
-"{\
-	\"type\": \"EXEC\", \
-	\"mbean\": \"org.apache.activemq.artemis:broker=\\\"0.0.0.0\\\",component=addresses,address=\\\"ExpiryQueue\\\",subcomponent=queues,routing-type=\\\"anycast\\\",queue=\\\"ExpiryQueue\\\"\", \
-	\"operation\": \"sendMessage(java.util.Map,int,java.lang.String,boolean,java.lang.String,java.lang.String)\", \
-	\"arguments\": [ {}, 1, \"test\", false, \"${ARTEMIS_USERNAME}\", \"${ARTEMIS_PASSWORD}\" ]\
-}")
-#echo ${POST_RESPONSE}
-POST_MESSAGE_ID=$(echo ${POST_RESPONSE} | jq ".value")
-POST_RESPONSE_STATUS=$(echo ${POST_RESPONSE} | jq ".status")
-echo "Post status: ${POST_RESPONSE_STATUS}"
-if [ "${POST_RESPONSE_STATUS}" != "200" ]
-then
-
-	# If producers are blocked
-	if (tail -100 /var/lib/artemis/log/artemis.log | grep "AMQ222212")
-	then 
-		# Logs it an exit. 
-		echo "System will start blocking producers"
-		exit 0;
-	else 
-		# Exits with an error.
-		exit "Message could not be added. Status: ${POST_RESPONSE_STATUS}"
-	fi
+health_check_success=false
+if bin/artemis producer \
+  --url tcp://localhost:61616 \
+  --user "${ARTEMIS_USERNAME}" \
+  --password "${ARTEMIS_PASSWORD}" \
+  --destination "health-check" \
+  --message-count 1 \
+  --message "Health check" \
+  && \
+  bin/artemis consumer \
+  --url tcp://localhost:61616 \
+  --user "${ARTEMIS_USERNAME}" \
+  --password "${ARTEMIS_PASSWORD}" \
+  --destination "health-check" \
+  --message-count 100 \
+  --receive-timeout 1000 \
+  --break-on-null
+then 
+    health_check_success=true
+    echo "Message added and received in the health check queue."
+else
+    echo "Failed to add/receive message to the health check queue."
+fi
 	
+# Ignores health check if the producers are blocked.
+if [ "${health_check_success}" != "true" ]
+then
+	if (tail -200 /var/lib/artemis/log/artemis.log | grep "AMQ222212")
+	then 
+   	 	echo "Ignoring health check because the producers are blocked."
+	else 
+		exit "Health check failed."
+	fi
 fi
 
-# Removes the test message from the queue.
-DELETE_RESPONSE=$(curl -f -s -u ${ARTEMIS_USERNAME}:${ARTEMIS_PASSWORD} -X POST -H "Origin: localhost" \
-	-H "Content-Type: application/json" "http://localhost:8161/console/jolokia/exec" -d \
-"{\
-	\"type\": \"EXEC\", \
-	\"mbean\": \"org.apache.activemq.artemis:broker=\\\"0.0.0.0\\\",component=addresses,address=\\\"ExpiryQueue\\\",subcomponent=queues,routing-type=\\\"anycast\\\",queue=\\\"ExpiryQueue\\\"\", \
-	\"operation\": \"removeMessage(long)\", \
-	\"arguments\": [ ${POST_MESSAGE_ID} ]\
-}")
-#echo ${DELETE_RESPONSE}
-DELETE_RESPONSE_STATUS=$(echo ${DELETE_RESPONSE} | jq ".status")
-echo "Delete status: ${DELETE_RESPONSE_STATUS}"
-if [ "${DELETE_RESPONSE_STATUS}" != "200" ]
-then
-	exit "Message could not be deleted. Status: ${DELETE_RESPONSE_STATUS}"
-fi
 
 
